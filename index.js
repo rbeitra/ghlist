a7a7e4c9c6d741f10e68215fe92c93f1bbb78ce1
Robin Beitra <robin@beitra.net>
2018-06-08 15:16:22 +0100

    Add display of created PRs

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ const addQueryToList = (q, status) => {
 
 const sortItems = (items) => {
   return items.sort((a, b) => {
+    //put our created items at the bottom
+    if (a.statuses.created !== b.statuses.created) {
+      return a.statuses.created ? 1:-1;
+    }
+    //put items awaiting an initial review at the top
     if (a.statuses.awaiting !== b.statuses.awaiting) {
       return a.statuses.awaiting ? -1:1;
     }
@@ -69,15 +74,23 @@ const printItems = () => {
     item.statuses.awaiting = (item.statuses.assigned || item.statuses.requested) && !item.statuses.reviewed;
   });
 
+  const iconAW = '=>'.red;
+  const iconRV = 'RV'.green;
+  const iconAS = 'AS'.green;
+  const iconRQ = 'RQ'.green;
+  const iconMN = 'MN'.green;
+  const iconCR = '<='.green;
+  const iconBLANK = '--'.grey;
+  const iconSPACE = '  '.grey;
   const itemsSorted = sortItems(allItems);
   itemsSorted.forEach((item)=>{
     console.log(sprintf(
-      `  ${'%2s  %2s  %2s  %2s  %2s'}  ${'%s'.yellow}  [${'%s'.red}]\n%44s[${'%-20s'.blue}      ${'%s'.magenta}]\n`,
-      item.statuses.awaiting&&' *'.red||'  '.grey,
-      item.statuses.reviewed&&'RV'.green||'--'.grey,
-      item.statuses.assigned&&'AS'.green||'--'.grey,
-      item.statuses.requested&&'RQ'.green||'--'.grey,
-      item.statuses.mentioned&&'MN'.green||'--'.grey,
+      `  ${'%2s  %2s  %2s  %2s  %2s'}  ${'%s'.yellow}  [${'%s'.red}\n%44s[${'%-20s'.blue}      ${'%s'.magenta}\n`,
+      (item.statuses.awaiting&&iconAW) || (item.statuses.created&&iconCR) || iconSPACE,
+      (item.statuses.reviewed&&iconRV) || iconBLANK,
+      (item.statuses.assigned&&iconAS) || iconBLANK,
+      (item.statuses.requested&&iconRQ) || iconBLANK,
+      (item.statuses.mentioned&&iconMN) || iconBLANK,
       item.updated_at,
       item.title,
       "",
@@ -85,14 +98,21 @@ const printItems = () => {
       item.html_url
     ));
   });
-  const awaitingReview = itemsSorted.filter((item) => item.statuses.awaiting);
-  console.log(`  ${(itemsSorted.length+'').magenta} open PRs.`);
-  console.log(`  ${(awaitingReview.length+'').red} awaiting review.`);
+  const awaiting = itemsSorted.filter((item) => item.statuses.awaiting);
+  const created = itemsSorted.filter((item) => item.statuses.created);
+  console.log(`  ${(itemsSorted.length+'').magenta} open PRs`);
+  console.log(`  ${(awaiting.length+'').red} awaiting review`);
+  console.log(`  ${(created.length+'').green} created`);
   console.log();
-  console.log("  (*=awaiting  RV=reviewed   AS=assigned   RQ=requested   MN=mentioned)".green);
+  console.log(sprintf(
+    '  (%2s:awaiting  %2s:reviewed   %2s:assigned   %2s:requested   %2s:mentioned  %2s:created)'
+      .split('%2s').map((str)=>str.grey).join('%2s'),
+      iconAW, iconRV, iconAS, iconRQ, iconMN, iconCR
+  ));
   console.log();
 };
 
+const createdQ = `is:open is:pr author:${apiUser} archived:false`;
 const reviewedQ = `is:open is:pr reviewed-by:${apiUser} archived:false`;
 const requestedQ = `is:open is:pr review-requested:${apiUser} archived:false`;
 const assignedQ = `is:open is:pr assignee:${apiUser} archived:false`;
@@ -106,6 +126,7 @@ spinner.start();
 
 //do all queries concurrently. resolve when they are all completed
 Promise.all([
+  addQueryToList(createdQ, "created"), 
   addQueryToList(reviewedQ, "reviewed"), 
   addQueryToList(requestedQ, "requested"),
   addQueryToList(assignedQ, "assigned"),
